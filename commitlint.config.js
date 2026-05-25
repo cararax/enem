fb70ded3f2ce8bfa96e8f79f0e9fe4cbc7b080df
Pedro Carara <cararax@gmail.com>
2026-05-25 14:09:20 -0300

    chore: initialize r project for enem data analysis
    - configure commitlint with husky for conventional commits
    - add .gitignore for r project artifacts
    - create directory structure (data/raw, data/processed, src, docs, output)
    - extend commit types: feat, fix, docs, style, refactor, test, chore, data, analysis, render

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'data',
+        'analysis',
+        'render',
+      ],
+    ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-max-length': [2, 'always', 72],
+  },
+}
